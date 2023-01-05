0077863dfb9f50d1039ce28b8a7d8651a14192af
abreutech <abreutech@hotmail.com>
2023-01-05 18:36:09 -0300

    fix: dbcontext

--- src/Q2P.Challenge.Data/DataContext.cs
+++ src/Q2P.Challenge.Data/DataContext.cs
@@ -5,6 +5,11 @@ namespace Q2P.Challenge.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext()
+        {
+
+        }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
@@ -12,13 +17,13 @@ namespace Q2P.Challenge.Data
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            base.OnModelCreating(modelBuilder);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=q2pchallengedb;Trusted_Connection=True;TrustServerCertificate=true;");
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=q2pdb;Trusted_Connection=True;TrustServerCertificate=true;");
         }
 
         public DbSet<Company> Companies { get; set; }
